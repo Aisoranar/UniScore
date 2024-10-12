d72bb72912f7bcd3f909af5a7bd7f77c33bd0442
Aisoranar <aisoranaya20@gmail.com>
2024-10-11 22:42:05 -0500

    actu

--- app/Http/Controllers/TraineeController.php
+++ app/Http/Controllers/TraineeController.php
@@ -28,11 +28,9 @@ class TraineeController extends Controller
     public function edit($id)
     {
         $profile = ProfileTrainee::where('user_id', $id)->firstOrFail();
-        $departments = DepartmentAndCity::select('id', 'department', 'city')->get()->groupBy('department');
 
         $data =[
             'profile'=> $profile,
-            'departments'=> $departments
         ];
 
         return view('view.trainee.profile', compact('data'));
@@ -43,27 +41,12 @@ class TraineeController extends Controller
         $profile = ProfileTrainee::where('user_id', $user_id)->firstOrFail();
 
         $profile->update($request->only([
-            'document_type',
-            'document_number',
-            'zone',
-            'birth_date',
-            'age',
-            'marital_status',
-            'has_children',
-            'address',
+            'name',
+            'surname',
+            'position',
+            'experience_level',
             'phone',
-            'department_id',
-            'city_id',
-            'health_regime',
-            'eps_name',
-            'sisben_classification',
-            'academic_program',
-            'schedule',
-            'disability',
-            'first_name',
-            'second_name',
-            'first_lastname',
-            'second_lastname'
+            
         ]));
 
         return redirect()->route('perfil.editar', ['id' => $profile->user_id])->with('success', 'Perfil actualizado exitosamente.');
